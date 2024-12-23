415aec69b3a7e0662573c27bc8ece93ac8c33d4f
Benjamin Kobjolke <b.kobjolke@xida.de>
2024-12-23 09:29:08 +0100

    IMPROVE: build proces

--- build.bat
+++ build.bat
@@ -1,5 +1,24 @@
+@echo off
+REM Clean build directory
+rmdir /s /q build 2>nul
 mkdir build
-del build/watchface.zip
-cd watchface
-7z a ../build\watchface.zip *.* -r
+
+REM Copy root files
+copy watchface\app.js build\
+copy watchface\app.json build\
+
+REM Copy assets folder
+xcopy /s /e /i watchface\assets build\assets
+
+REM Copy watchface folder
+xcopy /s /e /i watchface\watchface build\watchface
+
+REM Create zip archive from contents
+cd build
+7z a watchface.zip *.* assets\*.* watchface\*.* -r
+
+REM Clean up unzipped files
+for %%F in (*.*) do if not "%%F"=="watchface.zip" del "%%F"
+for /d %%D in (*) do rmdir /s /q "%%D"
+
 cd..
